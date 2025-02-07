9695f4cb0b6ee9179df93671ba6d25e21981282b
Muhammad Nauman <nauman33183@gmail.com>
2025-02-07 22:20:24 +0500

    Update contact.jsx

--- client/src/components/usercomponents/contact.jsx
+++ client/src/components/usercomponents/contact.jsx
@@ -58,6 +58,9 @@ const Form = () => {
       setLoading(true)
       const response = await fetch(`${backendURL}/contact`, {
         method: "POST",
+         headers: {
+          "Content-Type": "application/json"
+           },
         body: JSON.stringify(user)
       })
       const res_data = await response.json();
