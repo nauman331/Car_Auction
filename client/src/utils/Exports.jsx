0715c8896d3ba98701a335b2d4b0952c7d254bb7
Muhammad Nauman <nauman33183@gmail.com>
2025-03-26 20:13:29 +0500

    Update Exports.jsx

--- client/src/utils/Exports.jsx
+++ client/src/utils/Exports.jsx
@@ -1,5 +1,6 @@
 export const socketURL = "https://al-bashayera-car-auction-backend-2vat.onrender.com"
-export const backendURL = "https://al-bashayera-car-auction-backend.vercel.app/api";
+export const backendURL = "https://al-bashayera-car-auction-backend-2vat.onrender.com/api"
+// export const backendURL = "https://al-bashayera-car-auction-backend.vercel.app/api";
 // export const socketURL = "wss://abaautoauctions.com";
 // export const backendURL = "https://abaautoauctions.com/api";
 export const cloudinaryURL = "https://api.cloudinary.com/v1_1/dgdwau5vj/image/upload";
